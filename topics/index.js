500c90faa723b4ea4aba3290d1878855fcfa07de
Enrique Hernandez <enrique@doc.com>
2019-07-30 19:54:30 -0500

    Added opt convertion topics

--- topics/index.js
+++ topics/index.js
@@ -5,4 +5,7 @@ const config = { baseUrl: `${CAMUNDA_HOST}/engine-rest`, use: logger };
 
 const client = new Client(config);
 
-client.subscribe("creditScoreChecker", converters.opt2html);
+client.subscribe("opt2html", converters.opt2html);
+client.subscribe("opt2contribution", converters.opt2contribution);
+client.subscribe("opt2bundle", converters.opt2);
+client.subscribe("validateInstance", converters.opt2html);
